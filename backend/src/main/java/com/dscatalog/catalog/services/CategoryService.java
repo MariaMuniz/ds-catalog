dd6fab1afee51d4135cba510c538a7afdaeb8993
MariaMuniz <cidamuniz2011@yahoo.com.br>
2023-06-21 16:17:04 -0300

    Post new category

--- backend/src/main/java/com/dscatalog/catalog/services/CategoryService.java
+++ backend/src/main/java/com/dscatalog/catalog/services/CategoryService.java
@@ -26,11 +26,21 @@ public class CategoryService {
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		
 	}
+
 @Transactional(readOnly=true)
 public CategoryDTO findById(Long id) {
 	Optional<Category> obj = repository.findById(id);
 	Category entity = obj.orElseThrow(()-> new EntityNotFoundException("Entity not founld"));
 	
 	return new CategoryDTO(entity);
+}
+
+@Transactional(readOnly=true)
+public CategoryDTO insert(CategoryDTO dto) {
+Category entity = new Category();
+entity.setName(dto.getName());
+entity = repository.save(entity);
+return new CategoryDTO(entity);
+
 }
 }
