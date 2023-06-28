fed65c8f529d4793dcba00b3201773316975907a
MariaMuniz <cidamuniz2011@yahoo.com.br>
2023-06-28 16:31:00 -0300

    testes save optional

--- backend/src/test/java/com/dscatalog/catalog/repositories/ProductRepositoriesTests.java
+++ backend/src/test/java/com/dscatalog/catalog/repositories/ProductRepositoriesTests.java
@@ -3,23 +3,62 @@ package com.dscatalog.catalog.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import com.dscatalog.catalog.entities.Product;
+import com.dscatalog.catalog.tests.Factory;
 
 @DataJpaTest
 public class ProductRepositoriesTests {
 
 	@Autowired
 	private ProductRepository repository;
+	private long existingId;
+	private long nonExistingId;
+	private long countTotalProducts;
+	
+	@BeforeEach
+    void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId =1000L;
+		countTotalProducts = 25L;
+	}
+	
+	@Test
+	public void saveShouldPersitiWithAutoicrementWhenIdNull() {
+		Product product = Factory.createProduct();
+		product.setId(null);
+		product = repository.save(product);
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(countTotalProducts +1, product.getId());
+	}
+	
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
-		 long existingId= 1L;
-		 
+	
 		repository.deleteById(existingId);
 		Optional<Product> result = repository.findById(existingId);
 		Assertions.assertFalse(result.isPresent());
 	}
+	
+	@Test
+	public void notEmpatylShouldObjectWhenIdExists() {
+	
+		repository.findById(existingId);
+		Optional<Product> result = repository.findById(existingId);
+		Assertions.assertTrue(result.isPresent());
+	}
+	
+	@Test
+	public void EmpatylShouldObjectWhenIdNotExists() {
+	
+		repository.findById(nonExistingId);
+		Optional<Product> result = repository.findById(nonExistingId);
+		Assertions.assertFalse(result.isPresent());
+	}
+	
 }
---
+++ backend/src/test/java/com/dscatalog/catalog/tests/Factory.java
@@ -0,0 +1,22 @@
+package com.dscatalog.catalog.tests;
+
+import java.time.Instant;
+
+import com.dscatalog.catalog.dto.ProductDTO;
+import com.dscatalog.catalog.entities.Category;
+import com.dscatalog.catalog.entities.Product;
+
+public class Factory {
+	
+	
+	public static Product createProduct() {
+		Product product = new Product(1L, "Phone", "Good Phone",8000.0, "https://img.com/img.png",Instant.parse("2023-06-20T03:00:00Z"));
+        product.getCategories().add(new Category(2L, "Eletronics"));
+		return product;
+	}
+ public static ProductDTO createProductDTO() {
+	 Product product =createProduct();
+	 return new ProductDTO(product, product.getCategories());
+	 
+ }
+}
